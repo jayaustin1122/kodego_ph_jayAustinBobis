4d29a0fc184680f7d54543008014e6f627c72e4d
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2022-11-08 13:04:59 +0800

    Initial Commit

---
+++ src/main/kotlin/Activity 01 - A.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("-----------STUDENTS IN MD1P-----------")
+    var student1 : String = "Coco"
+    var student2 : String = "Martin"
+    var student3 : String = "Cardo"
+    var pro1 : String ="Incomplete"
+    var pro2 : String ="No Submissions"
+    var pro3 : String ="Complete"
+    println("----Student Lists----")
+    println(student1)
+    println(student2)
+    println(student3)
+    println("----------PROGRESS TAKEN----------")
+    println("$student1\n\t-is $pro1")
+    println("$student2\n\t-is $pro2")
+    println("$student3\n\t-is $pro3")
+}
